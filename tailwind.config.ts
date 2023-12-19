154c07b2c7adf5b1ed6c27a6b77c69987e672ee0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-19 10:06:31 +0100

    Feat: Added custom DaisyUI theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2EB875",
+          secondary: "#3BCE87",
+          accent: "#DEF7EB",
+          neutral: "#ffffff",
+          "base-100": "#ffffff",
+          info: "#62CCF9",
+          success: "#2CC98F",
+          warning: "#FBC337",
+          error: "#F76464",
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("daisyui")],
+};
+export default config;

061cf79fd193ab9b85f65c7d2aa2f51280a00e78
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-21 11:03:38 +0100

    Feat: Finished navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section>
-      <Layout>
         <Navbar />
         <section className="m-auto w-full">
           <div className="container mx-auto py-5 md:py-16 px-4  lg:flex lg:items-start overflow-x-hidden">
-           
+            <section className="hero flex items-center justify-center">
+              <h1 className="capitalize font-bold lg:text-4xl text-3xl">elevate your agro shopping experience</h1>
+            </section>
           </div>
         </section>
-      </Layout>
     </section>
   );
 }

5116df960fe6604808b9ff68832e324131048d56
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-21 09:01:30 +0100

    Feat: Finished redux setup

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,11 +1,24 @@
 "use client";
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { apiSlice } from "./features/api/api.slice";
+import authReducer from "./features/auth/auth.slice";
+import appReducer from "./features/app/app.slice";
 
 //import other reducerers
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    app: appReducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      apiSlice.middleware /** @todo socket middleware here*/
+    ),
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;

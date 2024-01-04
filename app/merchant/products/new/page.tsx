6110b4c943b19b92a62a3d65549622a55410bcd3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-04 08:53:55 +0100

    Feat: Finished add new product page

--- app/merchant/products/new/page.tsx
+++ app/merchant/products/new/page.tsx
@@ -4,69 +4,116 @@ import React, { useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { MerchantSidebarLayout } from "@/app/components/SidebarLayout";
 import { FaStar } from "react-icons/fa";
-import { BiBookmark, BiSolidBookmark } from "react-icons/bi";
 import Text from "@/app/components/Text";
 import Link from "next/link";
 import { FaPlus } from "react-icons/fa6";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/Input";
+import { FaUpload } from "react-icons/fa";
 
 const MerchantProductPage = () => {
   const pathname = usePathname();
 
   return (
     <MerchantSidebarLayout>
-      <form className="w-full lg:w-10/12">
+      <section className="w-full">
         <section>
           <h3 className="font-bold text-2xl capitalize text-accent">
             Add Product
           </h3>
           <Text className="text-sm">Add a new product to your store.</Text>
         </section>
+        <form className="w-full lg:w-10/12 mx-auto">
+          <br />
+          <section className="my-5">
+            <label className="block my-1">Product Name:</label>
+            <Input
+              type="text"
+              placeholder="Enter your product name"
+              name="productName"
+            />
+          </section>
 
-        <br />
-        <section className="my-5">
-          <label className="block my-1">Product Name:</label>
-          <Input
-            type="text"
-            placeholder="Enter your product name"
-            name="productName"
-          />
-        </section>
+          <section className="my-5">
+            <label className="block my-1">Product Description:</label>
+            <textarea className="textarea border-[1px] border-gray-300 focus:outline-none rounded-md w-full textarea-md" />
+          </section>
 
-        <section className="my-5">
-          <label className="block my-1">Product Description:</label>
-          <textarea className="textarea border-[1px] border-gray-300 focus:outline-none rounded-md w-full textarea-md" />
-        </section>
+          <section className="my-5">
+            <label className="block my-1">Product Price:</label>
+            <Input
+              type="number"
+              placeholder="Enter your product price"
+              name="productPrice"
+            />
+          </section>
 
-        <section className="my-5">
-          <label className="block my-1">Product Price:</label>
-          <Input
-            type="number"
-            placeholder="Enter your product price"
-            name="productPrice"
-          />
-        </section>
+          <section className="my-4 mb-5">
+            <label htmlFor="email" className="text-md block my-2">
+              Product Category
+            </label>
+            <select
+              className="select border-[1px] border-gray-300 focus:outline-none rounded-md w-full h-16 capitalize"
+              name="productCategory"
+            >
+              <option value="fruits & vegetables" className="capitalize">
+                fruits & vegetables
+              </option>
+              <option value="roots & tubers" className="capitalize">
+                roots & tubers
+              </option>
+              <option value="dairy products" className="capitalize">
+                dairy products
+              </option>
+              <option value="equipments" className="capitalize">
+                equipments
+              </option>
+              <option value="livestock supplies" className="capitalize">
+                livestock supplies
+              </option>
+              <option value="fruits & vegetables" className="capitalize">
+                fruits & vegetables
+              </option>
+              <option value="fertilizers" className="capitalize">
+                fertilizers
+              </option>
+              <option value="organic products" className="capitalize">
+                organic products
+              </option>
+              <option value="pesticides" className="capitalize">
+                pesticides
+              </option>
+              <option value="others" className="capitalize">
+                others
+              </option>
+            </select>
+          </section>
 
-        <section className="my-5">
-          <label className="block my-1">Product Images:</label>
-          <section className="w-full flex items-center justify-center my-1 border border-accent rounded h-20 p-2">
-            <section className="w-56 flex items-center">
-              <label className="capitalize text-white text-center  w-full rounded p-3 bg-primary transition-all transform hover:bg-accent cursor-pointer">
-                <input
-                  type="file"
-                  id="productImage"
-                  name="produtImages"
-                  className=""
-                  hidden
-                  multiple
-                />
-                upload images
-              </label>{" "}
+          <section className="my-5">
+            <label className="block my-1">Product Images:</label>
+            <section className="w-full flex items-center justify-center my-1 border border-accent rounded h-20 p-2">
+              <section className="w-56 flex items-center">
+                <label className="capitalize w-full cursor-pointer text-accent flex items-center justify-center font-bold">
+                  <input
+                    type="file"
+                    id="productImage"
+                    name="produtImages"
+                    className=""
+                    hidden
+                    multiple
+                  />
+                  <FaUpload className="text-accent w-5 h-5 text-center" />
+                </label>{" "}
+              </section>
             </section>
           </section>
-        </section>
-      </form>
+
+          <br />
+          <section className="my-4 mb-5  mx-auto">
+            <Button>Create product</Button>
+          </section>
+        </form>
+      </section>
     </MerchantSidebarLayout>
   );
 };

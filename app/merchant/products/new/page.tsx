501de9d5bc5f6b59dd407872e833fe4a9eaa3efe
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-04 08:17:06 +0100

    Feat: Updated merchant new product page

--- app/merchant/products/new/page.tsx
+++ app/merchant/products/new/page.tsx
@@ -8,40 +8,15 @@ import { BiBookmark, BiSolidBookmark } from "react-icons/bi";
 import Text from "@/app/components/Text";
 import Link from "next/link";
 import { FaPlus } from "react-icons/fa6";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/Input";
 
 const MerchantProductPage = () => {
   const pathname = usePathname();
-  const [totalProducts, setTotalProducts] = useState<number>(10);
-  const [productDetails, setProductDetails] = useState({
-    name: "Live Chicken",
-    description: "Fresh and healthy live chicken for sale.",
-    images: ["/assets/chicken.jpg", "/assets/potato.jpg", "/assets/grapes.jpg"],
-  });
-
-  const handleNameChange = (newName: string) => {
-    setProductDetails((prevDetails) => ({
-      ...prevDetails,
-      name: newName,
-    }));
-  };
-
-  const handleDescriptionChange = (newDescription: string) => {
-    setProductDetails((prevDetails) => ({
-      ...prevDetails,
-      description: newDescription,
-    }));
-  };
-
-  const handleImageAddition = (newImage: string) => {
-    setProductDetails((prevDetails) => ({
-      ...prevDetails,
-      images: [...prevDetails.images, newImage],
-    }));
-  };
 
   return (
     <MerchantSidebarLayout>
-      <section className="w-full">
+      <form className="w-full lg:w-10/12">
         <section>
           <h3 className="font-bold text-2xl capitalize text-accent">
             Add Product
@@ -49,53 +24,49 @@ const MerchantProductPage = () => {
           <Text className="text-sm">Add a new product to your store.</Text>
         </section>
 
-        <section className="my-4">
-          <label className="font-bold">Product Name:</label>
-          <input
+        <br />
+        <section className="my-5">
+          <label className="block my-1">Product Name:</label>
+          <Input
             type="text"
-            value={productDetails.name}
-            onChange={(e) => handleNameChange(e.target.value)}
-            className="w-full border p-2 rounded"
+            placeholder="Enter your product name"
+            name="productName"
           />
         </section>
 
-        <section className="my-4">
-          <label className="font-bold">Product Description:</label>
-          <textarea
-            value={productDetails.description}
-            onChange={(e) => handleDescriptionChange(e.target.value)}
-            className="w-full border p-2 rounded"
+        <section className="my-5">
+          <label className="block my-1">Product Description:</label>
+          <textarea className="textarea border-[1px] border-gray-300 focus:outline-none rounded-md w-full textarea-md" />
+        </section>
+
+        <section className="my-5">
+          <label className="block my-1">Product Price:</label>
+          <Input
+            type="number"
+            placeholder="Enter your product price"
+            name="productPrice"
           />
         </section>
 
-        <section className="my-4">
-          <label className="font-bold">Product Images:</label>
-          <section>
-            {productDetails.images.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Product Image ${index + 1}`}
-                className="w-20 h-20 object-cover m-2 rounded"
-              />
-            ))}
+        <section className="my-5">
+          <label className="block my-1">Product Images:</label>
+          <section className="w-full flex items-center justify-center my-1 border border-accent rounded h-20 p-2">
+            <section className="w-56 flex items-center">
+              <label className="capitalize text-white text-center  w-full rounded p-3 bg-primary transition-all transform hover:bg-accent cursor-pointer">
+                <input
+                  type="file"
+                  id="productImage"
+                  name="produtImages"
+                  className=""
+                  hidden
+                  multiple
+                />
+                upload images
+              </label>{" "}
+            </section>
           </section>
-          <input
-            type="text"
-            placeholder="Enter image URL"
-            className="w-full border p-2 rounded mt-2"
-            onChange={(e) => handleImageAddition(e.target.value)}
-          />
         </section>
-
-        <section className="flex items-center">
-          <Link href="/merchant/products">
-            <Link href="#" className="text-accent hover:underline">
-              <FaPlus className="mr-2" /> Add Another Product
-            </Link>
-          </Link>
-        </section>
-      </section>
+      </form>
     </MerchantSidebarLayout>
   );
 };

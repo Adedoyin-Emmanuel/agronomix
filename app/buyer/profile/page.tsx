348442a3a2634525bfc34234af2019774ac22edd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-06 04:06:01 +0100

    Feat: Implemented the modal confirmation for image upload

--- app/buyer/profile/page.tsx
+++ app/buyer/profile/page.tsx
@@ -13,9 +13,12 @@ import { AiOutlineCamera } from "react-icons/ai";
 import { useDispatch } from "react-redux";
 import { useUpdateBuyerMutation } from "@/app/store/features/app/app.slice";
 import Skeleton from "@/app/components/Skeleton/Skeleton";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Profile() {
   const dispatch = useDispatch();
+  const router = useRouter();
   const { userAuthInfo } = useAppSelector((state) => state.auth);
   const [updateBuyer, { isLoading }] = useUpdateBuyerMutation();
 
@@ -55,6 +58,9 @@ export default function Profile() {
     }
   };
 
+  const navigateToPicturePage = (): void => {
+    router.push("/buyer/profile/picture");
+  };
   return (
     <div className="w-screen h-screen">
       <SidebarLayout>
@@ -75,14 +81,36 @@ export default function Profile() {
                         className=""
                         src={userAuthInfo?.profilePicture}
                         alt="buyer profile image"
+                        onClick={navigateToPicturePage}
                       />
                     ) : (
                       <Skeleton className="w-24 h-24 rounded-full" />
                     )}
                   </div>
-                  <section className="pen-container bg-accent flex items-center justify-center rounded-full w-8 h-8 transform-gpu text-white translate-y-16 -translate-x-10 hover:scale-110 duration-100 ease-linear hover:bg-secondary hover:text-slate-200">
-                    <AiOutlineCamera className="h-6 w-6" />
-                  </section>
+                  <Link
+                    href="/buyer/profile/picture"
+                    className="bg-accent flex items-center justify-center rounded-full w-8 h-8 transform-gpu text-white translate-y-16 -translate-x-10 hover:scale-110 duration-100 ease-linear hover:bg-secondary hover:text-slate-200"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z"
+                      />
+                    </svg>
+                  </Link>
                 </div>
 
                 <form
--- app/buyer/profile/picture/page.tsx
+++ app/buyer/profile/picture/page.tsx
@@ -1,19 +1,124 @@
+"use client";
+import Button from "@/app/components/Button";
+import Input from "@/app/components/Input";
+import Loader from "@/app/components/Loader";
+import SidebarLayout from "@/app/components/SidebarLayout";
+import Text from "@/app/components/Text";
+import { useAppSelector } from "@/app/store/store";
+import React, { useState, useRef } from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useUpdateBuyerMutation } from "@/app/store/features/app/app.slice";
+import Skeleton from "@/app/components/Skeleton/Skeleton";
+import Modal from "@/app/components/Modal";
 
-"use client"
-import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+export default function Profile() {
+  const dispatch = useDispatch();
+  const { userAuthInfo } = useAppSelector((state) => state.auth);
+  const [updateBuyer, { isLoading }] = useUpdateBuyerMutation();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const modalRef = useRef<HTMLDialogElement | any>(null);
 
+  const showModal = () => {
+    if (modalRef && modalRef.current) {
+      modalRef?.current.showModal();
+    }
+  };
 
-const Index = () => {
-    const pathname = usePathname();
+  const closeModal = () => {
+    if (modalRef && modalRef.current) {
+      modalRef?.current.closeModal();
+    }
+  };
 
-    return (
-        <div>
-            <h1>Index works!</h1>
-            <p>Current pathname: {pathname}</p>
-        </div>
-    );  
+  const handleImageChange = (
+    event: React.ChangeEvent<HTMLInputElement> | any
+  ) => {
+    const file = event.target.files?.[0];
+    console.log(file);
+    if (file) {
+      setIsDialogOpen(true);
+      setSelectedImage(file);
+      setImageUrl(file ? URL.createObjectURL(file) : null);
+      console.log(URL.createObjectURL(file));
+      showModal();
+    } else {
+      toast.error("Please select an image");
+    }
+  };
+
+  const handleContinue = () => {
+    console.log("Uploading image:", selectedImage);
+    setIsDialogOpen(false);
+    closeModal();
+  };
+
+  const handleCancel = () => {
+    setSelectedImage(null);
+    setIsDialogOpen(false);
+  };
+
+  return (
+    <div className="w-screen h-screen">
+      <SidebarLayout>
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <section className="appointments my-5">
+            <h3 className="font-bold text-2xl capitalize text-accent">
+              Profile Picture
+            </h3>
+            <Text className="text-sm">Update your profile picture</Text>
+            <section className="update-profile w-full my-8">
+              <div className="image-section flex flex-col items-center justify-center">
+                <div className="avatar cursor-pointer">
+                  <div className="w-24 h-24 rounded-full overflow-hidden">
+                    {userAuthInfo?.profilePicture ? (
+                      <img
+                        className="w-full h-full object-cover"
+                        src={userAuthInfo?.profilePicture}
+                        alt="Buyer profile image"
+                      />
+                    ) : (
+                      <Skeleton className="w-full h-full rounded-full" />
+                    )}
+                  </div>
+                </div>
+
+                <Modal ref={modalRef}>
+                  <section className="">
+                    <section
+                      className="w-full flex items-center justify-center"
+                      my-2
+                    >
+                      <img src={imageUrl!} className="w-60 h-60 rounded-full" />
+                    </section>
+                    <section className="mt-8 w-full flex items-end justify-end">
+                      <Button>Proceed</Button>
+                    </section>
+                  </section>
+                </Modal>
+
+                <form className="w-60" onChange={handleImageChange}>
+                  <label className="capitalize w-full my-5 text-white flex items-center justify-center hover:bg-accent cursor-pointer rounded p-3 bg-primary transition-all transform">
+                    <input
+                      type="file"
+                      id="productImage"
+                      name="produtImages"
+                      className=""
+                      hidden
+                      multiple
+                    />
+                    upload
+                  </label>{" "}
+                </form>
+              </div>
+            </section>
+          </section>
+        )}
+      </SidebarLayout>
+    </div>
+  );
 }
-
-export default Index;
-    
